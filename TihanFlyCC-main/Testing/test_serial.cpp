3e03d08e67d720603d37d599551e8b1a59ec32b6
unknown <sufi03.ss@gmail.com>
2026-05-19 10:35:36 +0530

    Added unit testing changes

--- TihanFlyCC-main/Testing/test_serial.cpp
+++ TihanFlyCC-main/Testing/test_serial.cpp
@@ -3,54 +3,96 @@
 #include <thread>
 #include <atomic>
 #include <vector>
+#include "Transport/serial.h"
+
+#ifndef _WIN32
 #include <pty.h>
 #include <unistd.h>
-#include "serial.h"
 
-TEST(SerialTransportTest, PseudoTerminalTest)
-{
+class SerialTransportTest : public ::testing::Test {
+protected:
     int master_fd, slave_fd;
     char slave_name[100];
-
-    ASSERT_EQ(openpty(&master_fd, &slave_fd, slave_name, nullptr, nullptr), 0);
-
     asio::io_context io;
-
+    std::shared_ptr<SerialTransport> serial;
     std::atomic<bool> received{false};
     std::vector<uint8_t> received_data;
+    std::thread io_thread;
 
-    auto serial = std::make_shared<SerialTransport>(io, slave_name, 57600);
+    void SetUp() override {
+        ASSERT_EQ(openpty(&master_fd, &slave_fd, slave_name, nullptr, nullptr), 0);
+        serial = std::make_shared<SerialTransport>(io, slave_name, 57600);
+        serial->set_receive_callback(
+            [&](const uint8_t* data, std::size_t length) {
+                received = true;
+                received_data.assign(data, data + length);
+            });
+        serial->start();
+        io_thread = std::thread([&]() { io.run(); });
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    void TearDown() override {
+        serial->stop();
+        io.stop();
+        if (io_thread.joinable()) {
+            io_thread.join();
+        }
+        close(master_fd);
+        close(slave_fd);
+    }
     
+    void SendPacketAndWait(const std::vector<uint8_t>& data) {
+        write(master_fd, data.data(), data.size());
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+};
 
-    serial->set_receive_callback(
-        [&](const uint8_t* data, std::size_t length)
-        {
-            received = true;
-            received_data.assign(data, data + length);
-        });
+TEST_F(SerialTransportTest, DataIsReceived) {
+    SendPacketAndWait({9, 8, 7});
+    EXPECT_TRUE(received);
+}
 
-    serial->start();
+TEST_F(SerialTransportTest, ReceivedDataSizeIsCorrect) {
+    SendPacketAndWait({9, 8, 7});
+    EXPECT_EQ(received_data.size(), 3);
+}
 
-    std::thread io_thread([&]() {
-        io.run();
+TEST_F(SerialTransportTest, ReceivedDataFirstByteMatches) {
+    SendPacketAndWait({9, 8, 7});
+    if(!received_data.empty()) {
+        EXPECT_EQ(received_data[0], 9);
+    }
+}
+#endif
+
+// This test runs on all platforms (including Windows)
+// Split into multiple tests to adhere to Single-Assertion rule
+
+TEST(SerialTransportInvalidPortTest, IsOpenIsFalse) {
+    asio::io_context io;
+    auto serial = std::make_shared<SerialTransport>(io, "INVALID_COM_PORT", 57600);
+    EXPECT_FALSE(serial->is_open());
+}
+
+TEST(SerialTransportInvalidPortTest, IsActiveIsFalse) {
+    asio::io_context io;
+    auto serial = std::make_shared<SerialTransport>(io, "INVALID_COM_PORT", 57600);
+    EXPECT_FALSE(serial->is_active());
+}
+
+TEST(SerialTransportInvalidPortTest, StartDoesNotCrash) {
+    asio::io_context io;
+    auto serial = std::make_shared<SerialTransport>(io, "INVALID_COM_PORT", 57600);
+    EXPECT_NO_THROW({
+        serial->start();
     });
+}
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-    uint8_t test_data[] = {9, 8, 7};
-    write(master_fd, test_data, sizeof(test_data));
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
-
-    serial->stop();
-    io.stop();
-    io_thread.join();
-
-    close(master_fd);
-    close(slave_fd);
-
-    ASSERT_TRUE(received);
-    ASSERT_EQ(received_data.size(), 3);
-    EXPECT_EQ(received_data[0], 9);
-    EXPECT_EQ(received_data[2], 7);
+TEST(SerialTransportInvalidPortTest, StopDoesNotCrash) {
+    asio::io_context io;
+    auto serial = std::make_shared<SerialTransport>(io, "INVALID_COM_PORT", 57600);
+    EXPECT_NO_THROW({
+        serial->stop();
+    });
 }
